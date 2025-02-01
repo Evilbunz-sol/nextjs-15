cecd0bfd39a63cfbeb4da39c0d2e83eca62e5785
Evilbunz <jameel.altamash@gmail.com>
2025-02-01 16:30:47 -0500

    fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1 className="text-primary-500 text-3xl">Meow</h1>
-  );
+    <>
+      <h1 className="text-primary-500 text-3xl">Meow</h1>
+      <h1 className="text-primary-500 text-3xl font-spaceGrotesk"> Meow</h1>
+    </>
+  )
 }
