b9149c9ad35c541c79a6dfeb1a1958a0ecf8cdd8
Evilbunz <jameel.altamash@gmail.com>
2025-02-04 22:33:06 -0500

    oauth part 2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+"use client"
+
+import { auth } from '@/auth'
+import { Button } from '@/components/ui/button'
+import ROUTES from '@/constants/routes'
+import { signOut } from 'next-auth/react'
+
+const Home = () => {
+  const handleSignOut = async () => {
+    await signOut({ callbackUrl: ROUTES.SIGN_IN })
+  }
+
   return (
     <>
-      <h1 className="text-3xl"></h1>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <div className="px-10 pt-[100px]">
+        <Button onClick={handleSignOut}>Log Out</Button>
+      </div>
     </>
   )
 }
+
+export default Home
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
 import ThemeProvider from '@/context/Theme'
+import { Toaster } from '@/components/ui/toaster'
+import { SessionProvider } from 'next-auth/react'
+import { auth } from '@/auth'
+import React from 'react'
 
 const inter = localFont({
   src: './fonts/interVF.ttf', // Adjust the path based on your project structure
@@ -26,16 +30,22 @@ export const metadata: Metadata = {
   }
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth()
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme='system' enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </body>
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   )
 }
+
+export default RootLayout
