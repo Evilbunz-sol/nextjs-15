56a5a1a3e4cdbabae0c76dcd0e1684099c674adb
Evilbunz <jameel.altamash@gmail.com>
2025-02-05 21:21:01 -0500

    mobile left nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,15 @@
 "use client"
 
-import { Button } from '@/components/ui/button'
-import ROUTES from '@/constants/routes'
-import { signOut } from 'next-auth/react'
+
 
 const Home = () => {
-  const handleSignOut = async () => {
-    await signOut({ callbackUrl: ROUTES.SIGN_IN })
-  }
+
 
   return (
     <>
       <h1 className="h1-bold">Welcome to the world of Next.js</h1>
       <div className="px-10 pt-[100px]">
-        <Button onClick={handleSignOut}>Log Out</Button>
+
       </div>
     </>
   )
