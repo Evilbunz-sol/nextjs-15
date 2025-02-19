74f62b7933aba2d9330459e945a38c36797b6a98
Evilbunz <jameel.altamash@gmail.com>
2025-02-19 00:30:12 -0500

    logout

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -1,6 +1,9 @@
+import { LogOut } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { auth, signOut } from '@/auth'
+import { Button } from '@/components/ui/button'
 import {
   Sheet,
   SheetClose,
@@ -8,11 +11,14 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
-import { Button } from '@/components/ui/button'
 import ROUTES from '@/constants/routes'
+
 import NavLinks from './NavLinks'
 
-const MobileNavigation = () => {
+const MobileNavigation = async () => {
+  const session = await auth()
+  const userId = session?.user?.id
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -24,7 +30,6 @@ const MobileNavigation = () => {
           className="invert-colors sm:hidden"
         />
       </SheetTrigger>
-
       <SheetContent
         side="left"
         className="background-light900_dark200 border-none">
@@ -36,8 +41,9 @@ const MobileNavigation = () => {
             height={23}
             alt="Logo"
           />
+
           <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
-            Dev <span className="text-primary-500">Flow</span>
+            Dev<span className="text-primary-500">Flow</span>
           </p>
         </Link>
 
@@ -49,29 +55,46 @@ const MobileNavigation = () => {
           </SheetClose>
 
           <div className="flex flex-col gap-3">
-            <SheetClose asChild>
-              <Link href={ROUTES.SIGN_IN}>
-                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-                  <span className="primary-text-gradient">Log In</span>
-                </Button>
-              </Link>
-            </SheetClose>
+            {userId ? (
+              <SheetClose asChild>
+                <form
+                  action={async () => {
+                    'use server'
 
-            <SheetClose asChild>
-              <Link href={ROUTES.SIGN_UP}>
-                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-                  Sign Up
-                </Button>
-              </Link>
-            </SheetClose>
+                    await signOut()
+                  }}>
+                  <Button
+                    type="submit"
+                    className="base-medium w-fit !bg-transparent px-4 py-3">
+                    <LogOut className="size-5 text-black dark:text-white" />
+                    <span className="text-dark300_light900">Logout</span>
+                  </Button>
+                </form>
+              </SheetClose>
+            ) : (
+              <>
+                <SheetClose asChild>
+                  <Link href={ROUTES.SIGN_IN}>
+                    <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                      <span className="primary-text-gradient">Log In</span>
+                    </Button>
+                  </Link>
+                </SheetClose>
+
+                <SheetClose asChild>
+                  <Link href={ROUTES.SIGN_UP}>
+                    <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                      Sign Up
+                    </Button>
+                  </Link>
+                </SheetClose>
+              </>
+            )}
           </div>
         </div>
       </SheetContent>
     </Sheet>
   )
 }
+
 export default MobileNavigation
-
-
-
-
